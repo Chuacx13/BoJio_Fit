98cd521b05fc690968fad673412a8f3c31e4d56e
Chuacx13 <chuachongxuan@gmail.com>
2024-03-29 23:53:29 +0800

    Updated Home Page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,19 @@
 <template>
     <Navbar/>
-    <div class = "home-view" v-if = "user">
-        <h3>This is the home Page</h3>
+    <div class="home-view" v-if="user">
+        <div class="promotional-text">
+            <p>UNLEASH YOUR MIND.</p>
+            <p>UNLOCK YOUR POTENTIAL.</p>
+        </div>
+        <div class="tracking-text">
+            <p>START TRACKING YOUR WORKOUTS TODAY!</p>
+        </div>
     </div>
-    <div class = "home-view" v-else = "user">
+    <div class="home-view" v-else>
         <h3>Sign Up Now</h3>
     </div>
 </template>
-  
+
 <script>
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import Navbar from '@/components/Navbar.vue'
@@ -37,18 +43,48 @@ export default {
 </script>
 
 <style scoped>
+.navbar {
+    display: flex;
+    justify-content: center; 
+    align-items: center;
+    height: 120px; 
+    width: 100%;
+    position: fixed;
+    top: 100;
+    left: 0;
+    background: transparent; 
+}
+
 .home-view {
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   width: 100%;
-  min-height: 100vh; /* This will make sure the .home-view takes minimum of the full viewport height */
+  min-height: 100vh; 
   background-image: url('@/assets/home_background.png');
   background-size: cover;
   background-position: center center; 
   background-repeat: no-repeat; 
   background-attachment: fixed;
   text-align: center; 
+}
+
+.promotional-text p {
+    -webkit-text-stroke: 2px orange; 
+    color: transparent;
+    font-size: 4em; 
+    margin: 0; 
+    text-align: center;
+    font-weight: bold; 
+    white-space: nowrap; 
+}
+
+.tracking-text p {
+    color: white;
+    font-size: 2.2em; 
+    font-weight: bold; 
+    margin: 1; 
+    white-space: nowrap; 
 }
 </style>
