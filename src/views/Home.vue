79f4150960d27104da1b851c40409e52cb63cfb9
Chuacx13 <chuachongxuan@gmail.com>
2024-03-29 23:40:33 +0800

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,13 @@
 <template>
     <Navbar/>
-    <div class="home-view" v-if="user">
-        <div class="promotional-text">
-            <p>UNLEASH YOUR MIND.</p>
-            <p>UNLOCK YOUR POTENTIAL.</p>
-        </div>
-        <div class="tracking-text">
-            <p>START TRACKING YOUR WORKOUTS TODAY!</p>
-        </div>
+    <div class = "home-view" v-if = "user">
+        <h3>This is the home Page</h3>
     </div>
-    <div class="home-view" v-else>
+    <div class = "home-view" v-else = "user">
         <h3>Sign Up Now</h3>
     </div>
 </template>
-
+  
 <script>
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import Navbar from '@/components/Navbar.vue'
@@ -43,18 +37,6 @@ export default {
 </script>
 
 <style scoped>
-.navbar {
-    display: flex;
-    justify-content: center; 
-    align-items: center;
-    height: 120px; 
-    width: 100%;
-    position: fixed;
-    top: 100;
-    left: 0;
-    background: transparent; 
-}
-
 .home-view {
   display: flex;
   flex-direction: column;
@@ -68,23 +50,5 @@ export default {
   background-repeat: no-repeat; 
   background-attachment: fixed;
   text-align: center; 
-}
-
-.promotional-text p {
-    -webkit-text-stroke: 2px orange; 
-    color: transparent;
-    font-size: 4em; 
-    margin: 0; 
-    text-align: center;
-    font-weight: bold; 
-    white-space: nowrap; 
-}
-
-.tracking-text p {
-    color: white;
-    font-size: 2.2em; 
-    font-weight: bold; 
-    margin: 1; 
-    white-space: nowrap; 
 }
 </style>
