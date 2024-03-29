7cc76e69dca9dd594e79a655a63db7114d3e1f1a
Benjamin <e0957888@u.nus.edu>
2024-03-29 17:05:34 +0800

    Modified Home Page and Nav Bar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,14 +1,15 @@
 <template>
     <nav class="navbar" v-if = "user">
-        <router-link class="nav-route" to="/" exact>Home</router-link>
-        <router-link class="nav-route" to="/tracking" exact>Tracking</router-link>
-        <router-link class="nav-route" to="/history" exact>History</router-link>
-        <router-link class="nav-route" to="/searchUsers" exact>Search Users</router-link>
-        <router-link class="nav-route" to="/friendRequests" exact>Friend Requests</router-link>
-        <router-link class="nav-route" to="/feed" exact>Feed</router-link>
-        <router-link class="nav-route" to="/friends" exact>Friends</router-link>
-        <router-link class="nav-route" to="/profile" exact>Profile</router-link>
-        <button id = "btn" @click="signOut()"> Logout </button>
+        <img class="navbar-logo" :src="logo" alt="BoJio Fit Logo"> 
+        <router-link class="nav-route" to="/" exact>HOME</router-link>
+        <router-link class="nav-route" to="/tracking" exact>TRACKING</router-link>
+        <router-link class="nav-route" to="/history" exact>HISTORY</router-link>
+        <router-link class="nav-route" to="/searchUsers" exact>SEARCH USERS</router-link>
+        <router-link class="nav-route" to="/friendRequests" exact>FRIEND REQUESTS</router-link>
+        <router-link class="nav-route" to="/feed" exact>FEED</router-link>
+        <router-link class="nav-route" to="/friends" exact>FRIENDS</router-link>
+        <router-link class="nav-route profile-link" to="/profile" exact>PROFILE</router-link>
+        <button id = "btn" @click="signOut()"> LOGOUT </button>
     </nav>
     <nav class="navbar" v-else>
         <router-link class="nav-route" to="/" exact>Home</router-link>
@@ -19,12 +20,14 @@
 
 <script>
 import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
+import logo from '@/assets/logo.png';
 export default {
     name: 'Navbar',
 
     data() {
         return {
             user: false,
+            logo: logo, 
         }
     }, 
 
@@ -52,45 +55,51 @@ export default {
 <style>
 .navbar {
     display: flex;
-    justify-content: flex-end;
+    justify-content: space-between; 
     align-items: center;
-    height: 4rem;
-    background-color: white;
-    color: black;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-    font-family: monospace;
-}
-
-.nav-route {
-    display: block;
-    color: black;
-    text-align: center;
-    padding: 14px 20px;
-    text-decoration: none;
-    transition: padding 0.3s, background-color 0.3s;
-    border-radius: 8px;
-}
-
-.nav-route:hover {
-    color: white;
-    background-color: rgb(195, 195, 188);
-}
-
-#btn {
-    padding: 14px 20px;
-    font-size: 12px;
+    width: 100%;
+    position: fixed; 
+    z-index: 1000; 
     background-color: transparent;
-    color: black;
+}
+.nav-route.router-link-active {
+    color: orange; 
+}
+.navbar-logo {
+    height: 150px; 
+    width: auto; 
+    margin-top: 20px; 
+}
+.nav-route, #btn {
+    color: white;
+    padding: 20px 50px 20px 50px; 
+    text-decoration: none;
+    font-family: monospace;
+    transition: background-color 0.3s, color 0.3s;
+    font-size: 20px; 
+    font-weight: bold; 
+}
+.nav-route:hover, #btn:hover {
+    background-color: rgba(255, 255, 255, 0.2); 
+    color: white; 
+}
+#btn {
+    padding: 20px 50px 20px 50px;
+    font-size: 20px;
+    background-color: transparent;
+    color: white;
     border: none;
     cursor: pointer;
     text-decoration: none;
     transition: padding 0.3s, background-color 0.3s;
-    border-radius: 8px;
     font-family: monospace;
+    font-weight: bold; 
 }
-
 #btn:hover {
     color: white;
     background-color: rgb(195, 195, 188);
+}
+.profile-link {
+    border: 1px solid white;
 }
 </style>
