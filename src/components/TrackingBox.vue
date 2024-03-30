83eaa8096d0d4279cab4d9e4d9bad2c613987944
Chuacx13 <chuachongxuan@gmail.com>
2024-03-30 23:09:29 +0800

    Added design of Tracking Boxes

---
+++ src/components/TrackingBox.vue
@@ -0,0 +1,73 @@
+<template>
+    <div id="tracking-box">
+        <form @submit.prevent="saveWorkout">
+            <input type="text" id="name-input" v-model="exerciseName" placeholder="Name of Exercise" required>
+            <div id="workout-details">
+                <input type="number" id="weight-input" v-model="weight" placeholder="Weight" required>
+                <input type="number" id="reps-input" v-model="reps" placeholder="Reps" required>
+            </div>
+            <div id="tracking-buttons">
+                <button type="button" id="add-button"> Add Set </button>
+                <button type="button" id="delete-button"> Delete Set </button>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "TrackingBox",
+    methods: {}
+}
+</script>
+
+<style scoped>
+#tracking-box {
+    display: flex;
+    justify-content: center;
+    text-align: center;
+    padding: 20px;
+    background: white;
+    border-radius: 8px;
+}
+
+#workout-details, #tracking-buttons {
+    display: flex;
+    justify-content: center;
+}
+
+#name-input {
+    border-radius: 8px;
+    margin: 10px;
+    padding: 6px;
+}
+
+#weight-input, #reps-input {
+    border-radius: 8px;
+    width: 100px;
+    margin: 10px;
+    padding: 6px;
+}
+
+#add-button {
+    background-color: orange;
+    border-radius: 10px;
+    border: none;
+    color: white;
+    padding: 6px;
+    margin: 10px;
+    cursor: pointer;
+    width: 100px;
+}
+
+#delete-button {
+    background-color: red;
+    border-radius: 10px;
+    border: none;
+    color: white;
+    padding: 6px;
+    margin: 10px;
+    cursor: pointer;
+    width: 100px;
+}
+</style>
--- src/views/Tracking.vue
+++ src/views/Tracking.vue
@@ -1,14 +1,19 @@
 <template>
-    <div class = "tracking-view">
-        <h3>This is the Tracking Page</h3>
+    <div class="tracking-view">
+        <h1 class="tracking-header"> Workout </h1>
+        <TrackingBox/>
     </div>
 </template>
   
 <script>
 import { getAuth, onAuthStateChanged } from "firebase/auth";
+import TrackingBox from '@/components/TrackingBox.vue';
 
 export default {
     name: "Tracking",
+    components: {
+        TrackingBox
+    },
 
     data() {
         return {
@@ -28,5 +33,16 @@ export default {
 </script>
 
 <style scoped>
+.tracking-view {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background-color: rgb(46, 46, 46);
+    height: 100vh;
+}
 
+.tracking-header {
+    color: orange;
+    margin-top: 150px;
+}
 </style>
