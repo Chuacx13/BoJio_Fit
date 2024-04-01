b57706661ee4d98c52e84210049441584be9c701
Chuacx13 <chuachongxuan@gmail.com>
2024-04-02 00:47:22 +0800

    Added loading component to every page, stop loading when user state is established

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,36 @@
 <template> 
-    <Navbar/>
+    <Navbar v-if="!isLoading"/>
+    <Loading v-if="isLoading"/>
     <router-view/>
 </template> 
 
 <script> 
 import Navbar from './components/Navbar.vue';
+import Loading from './components/Loading.vue';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
 export default {
     name: 'App',
     components: {
-        Navbar
-    }
+        Navbar,
+        Loading
+    },
+    data() {
+        return {
+            isLoading: true,
+            user: false
+        }
+    },
+    mounted() {
+        const auth = getAuth();
+        onAuthStateChanged(auth, (user) => {
+            this.isLoading = true;
+            if (user) {
+                this.user = user;
+            }
+            this.isLoading = false;
+        })
+    },
 }
 </script>
 
