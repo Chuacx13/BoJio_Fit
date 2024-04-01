552f5ff5e250024337bfbf0797b9a9a03f37106e
Chuacx13 <chuachongxuan@gmail.com>
2024-04-02 00:22:04 +0800

    Added Loading Component

---
+++ src/components/Loading.vue
@@ -0,0 +1,28 @@
+<template> 
+    <div id="loading">
+        <img :src="loading" alt="loading"/>
+    </div>
+</template>
+
+<script> 
+import loading from '@/assets/loading.gif';
+
+export default {
+    name: 'Loading',
+
+    data() {
+        return {
+            loading: loading
+        }
+    }
+}
+</script>
+
+<style scoped> 
+#loading {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+}
+</style>
