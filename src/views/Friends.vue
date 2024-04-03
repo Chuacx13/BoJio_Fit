20cd321f810e4a759923972c4b24d2d1fd0ea38f
Sia Zi Xuan <zixuan@ZX-MacBook-Air.local>
2024-04-04 06:10:01 +0800

    designed friends, friendRequests, SearchUsers page

--- src/views/Friends.vue
+++ src/views/Friends.vue
@@ -1,11 +1,16 @@
 <template>
     <div class = "friends-view">
-        <h3>This is the Friends Page</h3>
+        <h1 class="friends-header"> Friends </h1>
+        <ul v-if="friends.length > 0" v-for="friend in friends" class="friends-display">
+            {{ friend.username }} is your friend.
+        </ul>
+        <p v-else class="friends-display">You have no friends currently...</p>
     </div>
 </template>
   
 <script>
 import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { getFirestore, doc, getDoc } from 'firebase/firestore';
 
 export default {
     name: "Friends",
@@ -13,6 +18,7 @@ export default {
     data() {
         return {
             user: false,
+            friends: [],
         }
     }, 
 
@@ -21,12 +27,62 @@ export default {
         onAuthStateChanged(auth, (user) => {
             if (user) {
                 this.user = user;
+                this.fetchFriends();
             }
         })
     },
+
+    methods: {
+        async fetchFriends() {
+            const db = getFirestore();
+            const userDocRef = doc(db, 'Users', this.user.uid);
+            const userDocSnapshot = await getDoc(userDocRef);
+            try {
+                if (userDocSnapshot.exists()) {
+                    const userData = userDocSnapshot.data();
+                    this.friends = userData.friends;
+                    console.log(this.friends);
+                } else {
+                    console.log('error loading friends');
+                }
+            } catch(error) {
+                console.log(error);
+            }
+        }
+    }
 }
 </script>
 
 <style scoped>
+.friends-view {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+    background-color: rgb(46, 46, 46);
+    min-height: 100vh;
+}
 
+.friends-header {
+    position: absolute; 
+    top: 0%; 
+    left:44%; 
+    color: orange;
+    align-items: center;
+    margin-top: 120px;
+    margin-bottom: 40px;
+}
+
+.friends-display {
+    position: absolute; 
+    font-size: 20px;
+    top: 15%; 
+    right: 25%; 
+    display: flex;
+    align-items: center;
+    width: 50%;
+    justify-content: center;
+    margin: 40px;
+    color: orange;
+}
 </style>
--- src/views/SearchUsers.vue
+++ src/views/SearchUsers.vue
@@ -1,14 +1,20 @@
 <template>
     <div class = "searchUsers-view">
-        <h3>This is the Search Users Page</h3>
+        <h1 class="searchUsers-header"> Search Users </h1>
+        <SearchUsersBar/>
     </div>
 </template>
   
 <script>
 import { getAuth, onAuthStateChanged } from "firebase/auth";
+import SearchUsersBar from '@/components/SearchUsersBar.vue';
 
 export default {
     name: "SearchUsers",
+
+    components: {
+        SearchUsersBar
+    },
 
     data() {
         return {
@@ -28,5 +34,19 @@ export default {
 </script>
 
 <style scoped>
+.searchUsers-view {
+    display: flex;
+    background-color: rgb(46, 46, 46);
+    min-height: 100vh;
+}
 
+.searchUsers-header {
+    position: absolute; 
+    top: 0%; 
+    left:44%; 
+    color: orange;
+    align-items: center;
+    margin-top: 120px;
+    margin-bottom: 40px;
+}
 </style>
