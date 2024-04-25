d0502465f54f39200dad198bd73a44c4fc1c485b
Benjamin <e0957888@u.nus.edu>
2024-04-25 17:05:49 +0800

    Feed Page Modification

--- src/views/Feed.vue
+++ src/views/Feed.vue
@@ -1,29 +1,40 @@
 <template>
-    <div class="feed-view">
-        <h1 class="feed-header">Workout Feed</h1>
-        <div v-if="workouts.length === 0" class="no-data">
-            No workouts to display
+    <div class="container">
+      <div class="left-container">
+        <div class="left-text">
+          <h3><span class="orange-text">WORKOUT</span><br/><span class="white-text">FEED</span></h3>
         </div>
-        <div v-else v-for="workout in sortedWorkouts" :key="workout.id" class="workout-entry">
-            <div class="workout-details">
-                <h3>{{ workout.username }} {{ workout.workoutName }} on {{ workout.date }}</h3>
-                <p><strong>Duration:</strong> {{ workout.duration }} minutes</p>
-                <p><strong>Exercises:</strong></p>
+        <div class="encouragement-text">
+          <p>View the latest workouts<br>of your friends</p>
+        </div>
+      </div>
+  
+      <div class="right-container">
+        <div v-if="workouts.length === 0" class="no-workouts">
+          No workouts to display
+        </div>
+        <div v-else class="workout-entry" v-for="workout in sortedWorkouts" :key="workout.id">
+          <img :src="workout.profilePic || defaultProfilePic" class="profile-pic" alt="Profile Picture">
+          <div class="workout-details">
+            <h3>{{ workout.username }} {{ workout.workoutName }} on {{ workout.date }}</h3>
+            <p><strong>Duration:</strong> {{ workout.duration }} minutes</p>
+            <p><strong>Exercises:</strong></p>
+            <ul>
+              <li v-for="exercise in workout.exercises" :key="exercise.name">
+                {{ exercise.name }}
                 <ul>
-                    <li v-for="exercise in workout.exercises" :key="exercise.name">
-                        {{ exercise.name }}
-                        <ul>
-                            <li v-for="(set, index) in exercise.sets" :key="index">
-                                Set {{ index + 1 }}: {{ set.weight }} Kg x {{ set.reps }} Reps
-                            </li>
-                        </ul>
-                    </li>
+                  <li v-for="(set, index) in exercise.sets" :key="index">
+                    Set {{ index + 1 }}: {{ set.weight }} Kg x {{ set.reps }} reps
+                  </li>
                 </ul>
-            </div>
-            <img :src="workout.profilePic || defaultProfilePic" class="profile-pic" alt="Profile Picture">
+              </li>
+            </ul>
+          </div>
         </div>
+      </div>
     </div>
-</template>
+  </template>
+  
 
 
 <script>
@@ -96,52 +107,98 @@ export default {
 </script>
 
 <style scoped>
-.feed-view {
+.container {
     display: flex;
-    flex-direction: column;
-    align-items: center;
-    background-color: #2E2E2E;
-    color: white;
     min-height: 100vh;
-    margin-bottom: 10vh;
+    background-color: rgb(46, 46, 46);
 }
 
-.feed-header {
+.left-container {
+    width: 40%; 
+    position: absolute;
+    display: flex; 
+    background-color: #2E2E2E;
+    height: 100%;
+    overflow-y: auto;
+}
+
+.left-text h3 {
+    position: absolute;
+    left: 10%;
+    top: 35%;  
+    font-size: 4vw;
+    margin: 0; 
+    text-align: left;
+    font-weight: bold;
+}
+
+.orange-text {
     color: orange;
-    font-size: 3vw;
+}
+
+.white-text {
+    color: white;
+}
+
+.encouragement-text { 
+    position: absolute;
+    display: flex; 
+    left: 10%;
+    top: 40%;
+    transform: translateY(100%);
+    text-align: left; 
+    color: white;
+    font-size: 1.1vw; 
+    padding-top: 10px;
+}
+    
+.right-container {
+  margin-top: 1vh;
+  margin-bottom: 10vh;
+  width: 50%; 
+  height: 80%; 
+  left: 40%; 
+  background-color: #2E2E2E;
+  color: white;
+  display: flex;
+  position: relative; 
+  flex-direction: column;
 }
 
 .workout-entry {
     display: flex;
-    justify-content: space-between; 
+    justify-content: space-between;
     align-items: center; 
-    background-color: #3E3E3E;
+    background-color: #3A3A3A;
+    border-radius: 2vw;
+    font-size: 1.2vw;
     padding: 1vw;
-    border-radius: 1vw;
-    margin: 0.5vh;
-    width: 70%;
+    margin: 1vw;
+    width: 80%;
 }
 
 .profile-pic {
-    width: 80px; 
-    height: 80px; 
+    width: 80px;
+    height: 80px;
     border-radius: 50%;
-    margin-right: 20px; 
-    order: 2; 
+    margin-left: 10px; 
+    margin-bottom: 25%;
 }
 
 .workout-details {
-    flex: 1;
-    font-size: 1vw;
-    order: 1; 
+    flex: 0.95; 
+    text-align: left;
 }
 
-.workout-entry h3 {
-    color: #FFA500;
-}
-
-.workout-entry p, li {
-    color: #FFFFFF;
+.no-workouts {
+    position: absolute; 
+    display: flex; 
+    color: white; 
+    font-size: 1vw; 
+    margin-left: 1vw; 
+    margin-top: 40%; 
 }
 </style>
+
+
 
